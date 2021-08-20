22703701d3de186f2cb89d640f80227e1d8a3a9f
Jose Luis Nava <jlgnava1986@gmail.com>
2021-08-20 17:10:33 +0800

    Updated about.blade.php file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,16 @@
     <div class="row">
         <div class="col-md-12">
             <h1>About Autism Strong Philippines</h1>
+            <br>
+            <p>Autism Strong Philippines is an advocacy project which promotes autism awareness in campuses, communities, schools, and workplaces across the country. It started by two adults on the spectrum as a dry-run project last June 2018; and officially launched December 2018. </p>
+            <p>The advocacy started with just a few materials - pencil, permanent marker and white cartolina. They tried out as a marketing campaign on autism awareness and anti-bullying. They soon shared their experiences as self-advocates.</p>
+            <br>
+            <h3>Mission Statement</h3>
+            <br>
+            <p>To strive towards acceptance and inclusion for persons with autism (PWAs), providing equity and accessibility for their rights and opportunities in tertiary institutions, in workplaces, and in various communities.</p>
+            <br>
+            <h3>Values</h3>
+            <p>Acceptance, Awareness, Empowerment, Equality, Friendship, Respect, Sensitivity, Understanding</p>
         </div>
     </div>
 </div>
