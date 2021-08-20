a7f9e86c0f585f2e0e916f5aa506741e28647801
Jose Luis Nava <jlgnava1986@gmail.com>
2021-08-20 13:47:50 +0800

    Updated show.blade.php with width and height attributes

--- resources/views/photos/show.blade.php
+++ resources/views/photos/show.blade.php
@@ -10,5 +10,5 @@
         <button type="submit" name="button" class="btn btn-danger">Delete</button>
     <a href="{{ route('gallery-show', $photo->gallery->id) }}" class="btn btn-info">Go Back</a>
     <hr>
-    <img src="/storage/gallery/{{ $photo->gallery_id }}/{{ $photo->photo }}" alt="{{ $photo->photo }}">
+    <img src="/storage/gallery/{{ $photo->gallery_id }}/{{ $photo->photo }}" alt="{{ $photo->photo }}" width="1366px" height="768px">
 @endsection
